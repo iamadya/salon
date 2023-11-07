fcddc7649a2e530ec60f95d3ac8f2631e5f3e40c
Adya Jha <adyajha2002@gmail.com>
2023-11-07 16:54:48 +0530

    screens added

---
+++ lib/screens/login_screen/login_screen.dart
@@ -0,0 +1,136 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../otp_screen/otp_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isLoading = false;
+  final phoneNumberController = TextEditingController();
+  final auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios_new),
+              onPressed: () {
+                exit(0);
+              })),
+      body: Container(
+        margin: EdgeInsets.only(left: 25, right: 25),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image.asset(
+              //   'assets/images/img3.png',
+              //   width: 160,
+              //   height: 160,
+              // ),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                "Phone Verification",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Let's get you registered!",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                        child: TextField(
+                      textAlign: TextAlign.center,
+                      controller: phoneNumberController,
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "+91 6845684142",
+                      ),
+                    ))
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding:
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 20)),
+                onPressed: () {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  auth.verifyPhoneNumber(
+                    phoneNumber: phoneNumberController.text,
+                    verificationCompleted: (_) {},
+                    verificationFailed: (FirebaseAuthException e) {
+                      setState(() {
+                        isLoading = false;
+                      });
+                      if (e.code == 'invalid-phone-number') {
+                        print('The provided phone number is not valid.');
+                      }
+                    },
+                    codeSent: (String verificationId, int? token) {
+                      setState(() {
+                        isLoading = true;
+                      });
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  OtpScreen(verificationId: verificationId)));
+                    },
+                    codeAutoRetrievalTimeout: (String verificationId) {
+                      // Auto-resolution timed out...
+                    },
+                  );
+                  print('code has been sent!');
+                },
+                child: isLoading
+                    ? SizedBox(
+                        child: CircularProgressIndicator(),
+                        height: 20,
+                        width: 20,
+                      )
+                    : Text("Send code"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/otp_screen/otp_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class OtpScreen extends StatelessWidget {
+  const OtpScreen({super.key, required String verificationId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context).pop();
+              })),
+      body: Container(
+        margin: EdgeInsets.only(left: 25, right: 25),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/img2.png',
+                width: 160,
+                height: 160,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                "Verification OTP",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "OTP has been sent to your phone",
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              OtpTextField(
+                numberOfFields: 6,
+                borderColor: Colors.black,
+                fieldWidth: 40,
+                borderRadius: BorderRadius.all(Radius.circular(11)),
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                //set to true to show as box or false to show as dash
+                showFieldAsBox: true,
+                //runs when a code is typed in
+                onCodeChanged: (String code) {
+                  //handle validation or checks here
+                },
+                //runs when every textfield is filled
+                onSubmit: (String verificationCode) {
+                  Navigator.pushNamed(context, '/profile_screen');
+                  Fluttertoast.showToast(
+                    msg: "Sign in successfully",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.green,
+                    textColor: Colors.white,
+                  );
+                }, // end onSubmit
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
