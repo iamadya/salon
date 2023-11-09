3737019af9f0f2e40d818c6e48cf8af0dcffd66e
Adya Jha <adyajha2002@gmail.com>
2023-11-09 23:05:59 +0530

    without button done

--- lib/screens/otp_screen/otp_screen.dart
+++ lib/screens/otp_screen/otp_screen.dart
@@ -1,31 +1,71 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class OtpScreen extends StatelessWidget {
-  const OtpScreen({Key? key, required String verificationId});
+  final String verificationId;
+
+  const OtpScreen({Key? key, required this.verificationId}) : super(key: key);
+
+  Future<void> signInWithOTP(String smsCode, BuildContext context) async {
+    try {
+      PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(
+        verificationId: verificationId,
+        smsCode: smsCode,
+      );
+
+      await FirebaseAuth.instance.signInWithCredential(phoneAuthCredential);
+
+      // If the verification is successful, navigate to the home screen
+      Navigator.pushNamed(context, '/home_screen');
+
+      Fluttertoast.showToast(
+        msg: "Sign in successfully",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+      );
+    } catch (e) {
+      // Handle the case when the entered OTP is incorrect or verification fails
+      Fluttertoast.showToast(
+        msg: "Invalid OTP. Please try again.",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.of(context).pop();
-              }),
-          actions: [
-            TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Skip',
-                  style: TextStyle(
-                    color: Colors.red,
-                    // decoration: TextDecoration.underline,
-                  ),
-                ))
-          ]),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              // Handle 'Skip' action
+            },
+            child: Text(
+              'Skip',
+              style: TextStyle(
+                color: Colors.red,
+              ),
+            ),
+          )
+        ],
+      ),
       body: Container(
         margin: EdgeInsets.only(left: 25, right: 25),
         alignment: Alignment.center,
@@ -69,35 +109,23 @@ class OtpScreen extends StatelessWidget {
                       borderRadius: BorderRadius.all(Radius.circular(11)),
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       showFieldAsBox: false,
-
                       onCodeChanged: (String code) {},
-                      //runs when every textfield is filled
                       onSubmit: (String verificationCode) {
-                        Navigator.pushNamed(context, '/home_screen');
-                        Fluttertoast.showToast(
-                          msg: "Sign in successfully",
-                          toastLength: Toast.LENGTH_SHORT,
-                          gravity: ToastGravity.BOTTOM,
-                          timeInSecForIosWeb: 1,
-                          backgroundColor: Colors.green,
-                          textColor: Colors.white,
-                        );
-                      }, // end onSubmit
+                        signInWithOTP(verificationCode, context);
+                      },
                     ),
                     SizedBox(height: 40),
                     SizedBox(
                       width: MediaQuery.of(context).size.width * 0.8,
                       height: 50,
                       child: FilledButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/home_screen');
-                        },
+                        onPressed: () {},
                         child: Text('Continue'),
                         style: ButtonStyle(
                           backgroundColor: MaterialStateProperty.all<Color>(
                               Color(0xFF024163)),
                           shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                          MaterialStateProperty.all<RoundedRectangleBorder>(
                             RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10),
                             ),
@@ -111,16 +139,17 @@ class OtpScreen extends StatelessWidget {
                       text: TextSpan(
                         text: 'By continuing you are agreeing to our\n',
                         style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 12,
-                            fontWeight: FontWeight.w500),
+                          color: Colors.black,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
                         children: [
                           TextSpan(
                             text: 'Terms & Conditions',
                             style: TextStyle(
                               color: Color(0xFF4082a0),
                               textBaseline: TextBaseline.alphabetic,
-                              fontSize: 13, // Adjust the font size as needed
+                              fontSize: 13,
                             ),
                             recognizer: TapGestureRecognizer()
                               ..onTap = () {
@@ -130,16 +159,17 @@ class OtpScreen extends StatelessWidget {
                           TextSpan(
                             text: ' and ',
                             style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                                fontWeight: FontWeight.w500),
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                           TextSpan(
                             text: 'Privacy Policy',
                             style: TextStyle(
                               color: Color(0xFF4082a0),
                               textBaseline: TextBaseline.alphabetic,
-                              fontSize: 13, // Adjust the font size as needed
+                              fontSize: 13,
                             ),
                             recognizer: TapGestureRecognizer()
                               ..onTap = () {
